b85612975174b1a5b7122a7028252c713880a9c3
Haiden Taylor <haiden@asciant.com>
2019-04-14 12:45:52 +1000

    Update the reducer to set a boolean for completed status

--- app/reducers/todos.js
+++ app/reducers/todos.js
@@ -1,14 +1,9 @@
-import * as Identicon from 'identicon.js';
 import * as Md5 from 'md5.js';
 
 function todoReducer(state = [], action) {
   const key = new Md5()
     .update(btoa(Math.random()).substring(0, 30))
     .digest('hex');
-  const icon = new Identicon(btoa(Math.random()).substring(0, 30), {
-    size: 64,
-    format: 'svg'
-  }).toString();
 
   switch (action.type) {
     case 'ADD_TODO': {
@@ -18,7 +13,7 @@ function todoReducer(state = [], action) {
         {
           task: action.task,
           date: Date.now(),
-          icon,
+          complete: false,
           key
         }
       ];
