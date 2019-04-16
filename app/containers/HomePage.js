351a50ce85f421ff6311dd2b074d5ed4c92fbf58
Haiden Taylor <haiden@asciant.com>
2019-04-16 14:33:52 +1000

    Connect up the Home component to redux

--- app/containers/HomePage.js
+++ app/containers/HomePage.js
@@ -1,13 +1,20 @@
-// @flow
-import React, { Component } from 'react';
+// App container for mapStateToProps and mapDispatchToState
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import * as actionCreators from '../actions/todos';
+
 import Home from '../components/Home';
 
-type Props = {};
-
-export default class HomePage extends Component<Props> {
-  props: Props;
-
-  render() {
-    return <Home />;
-  }
+function mapStateToProps(state) {
+  return state;
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(actionCreators, dispatch);
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home);
